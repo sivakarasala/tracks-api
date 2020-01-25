60c3e645fd3016d61a1050ace89e9f7516126e70
Siva Krishna <shivataraya@gmail.com>
2020-01-25 09:04:56 -0600

    wireup user model with mongoose

--- src/index.js
+++ src/index.js
@@ -1,9 +1,33 @@
+require("./models/User");
 const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const bodyParser = require("body-parser");
+const authRoutes = require("./routes/authRoutes");
+
+// Load env vars
+dotenv.config({ path: __dirname + "/config/config.env" });
+
+// Connect to database
+connectDB();
 
 const app = express();
+app.use(bodyParser.json());
+app.use(authRoutes);
 
+const port = process.env.PORT || 5000;
 app.get("/", (req, res) => {
   res.send("Aum Namah Shivaya");
 });
 
-app.listen(4000, console.log(`Aum Namah Shivaya`));
+const server = app.listen(
+  port,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
+);
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // Close server & exit process
+  server.close(() => process.exit(1));
+});
